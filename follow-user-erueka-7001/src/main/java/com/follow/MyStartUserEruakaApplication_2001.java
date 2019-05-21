3b642eded20ff65bd3f4f14157d75e6cc680c05c
422078082@qq.com <422078082@qq.com>
2019-05-21 16:39:24 +0800

    提交eureka

--- follow-user-erueka-7001/src/main/java/com/follow/MyStartUserEruakaApplication_2001.java
+++ follow-user-erueka-7001/src/main/java/com/follow/MyStartUserEruakaApplication_2001.java
@@ -11,11 +11,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class MyStartUserEruakaApplication
+public class MyStartUserEruakaApplication_2001
 {
     public static void main( String[] args )
     {
 
-        SpringApplication.run(MyStartUserEruakaApplication.class, args);
+        SpringApplication.run(MyStartUserEruakaApplication_2001.class, args);
     }
 }
---
+++ follow-user-erueka-7003/src/main/java/com/follow/MyStartUserEruakaApplication_7003.java
@@ -0,0 +1,21 @@
+package com.follow;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class MyStartUserEruakaApplication_7003
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(MyStartUserEruakaApplication_7003.class, args);
+    }
+}
