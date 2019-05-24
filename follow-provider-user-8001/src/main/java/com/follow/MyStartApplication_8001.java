4c56d89aab9a88df91154765330d145f37baa610
422078082@qq.com <422078082@qq.com>
2019-05-24 19:08:06 +0800

    提交eureka

--- follow-provider-user-8001/src/main/java/com/follow/MyStartApplication_8001.java
+++ follow-provider-user-8001/src/main/java/com/follow/MyStartApplication_8001.java
@@ -1,6 +1,5 @@
 package com.follow;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,11 +10,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class MyStartApplication
+public class MyStartApplication_8001
 {
     public static void main( String[] args )
     {
 
-        SpringApplication.run(MyStartApplication.class, args);
+        SpringApplication.run(MyStartApplication_8001.class, args);
     }
 }
