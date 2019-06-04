2aba5911e57a055e39c881369d25c4e58b0f47d0
422078082@qq.com <422078082@qq.com>
2019-06-04 20:18:55 +0800

    提交eureka

---
+++ follow-commom/src/main/java/com/follow/result/ResponseStatusCode.java
@@ -0,0 +1,24 @@
+package com.follow.result;
+
+public class ResponseStatusCode {
+
+    /**
+     * OK
+     */
+    public static final int OK = 0;
+
+    /**
+     * 未知异常
+     */
+    public static final int UNKNOW_EXCEPTION = 100;
+
+    /**
+     * 参数异常
+     */
+    public static final int ARGUMENT_EXCEPTION_104 = 104;
+
+    /**
+     * 自定义异常
+     */
+    public static final int ARGUMENT_EXCEPTION_404= 404;
+}
